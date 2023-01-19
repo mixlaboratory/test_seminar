b38b9c4738bf9715cedc9f7614b37c0532de93fc
KandaNaoki-1118 <naoki.kanda@mix-lab.net>
2023-01-19 13:11:18 +0900

    add file work11.c

--- work11.c
+++ work11.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
 int main(void){
-    printf("c20-902 神田直輝\n");
+    printf("c19-033 木村峻輔\n");
+<<<<<<< HEAD
+    printf("c19-037 國友竜暉\n");
+=======
+<<<<<<< HEAD
+    printf("c20-018 遠藤飛鳥\n");
+=======
+<<<<<<< HEAD
+    printf("c99-999 工大太郎\n");
+<<<<<<< HEAD
+    printf("c20-110 吉田瑞生\n");
+=======
+    printf("c20-007 稲岡秀和\n");
+=======
+    printf("c99-999 工大太郎２\n");
+<<<<<<< HEAD
+    printf("c20-020 小川虎次郎")
+=======
+    printf("c20-902 神田直輝\n")
+>>>>>>> a0a6dc3c782527e18fde1d4a6eafdeb45e1eac97
+>>>>>>> 1a4861045f88caae2b0698ff1c5ee6f0b05accb6
+>>>>>>> 6457683571871ac04df0e360b864f5d7ac218298
+>>>>>>> fe1e4e2d700823b1d35459c409ba4700d9dea8ca
     return 0;
 }
