818349b0bc0e703efbdccc04343b03cee06de9c3
LAPTOP-QKNUNLA7\miyuk <ESPwxj36#&>
2023-12-25 10:05:24 +0900

    2023.12.25 9:58 高梨美雪 関数追加

--- sample.py
+++ sample.py
@@ -8,6 +8,16 @@ def reverse_string(input_str):
 def capitalize_string(input_str):
     return input_str.capitalize()
 
+#数字を計算して結果を返す
+def shiki(x, y, z):
+    intx = int(x)
+    inty = int(y)
+    intz = int(z)
+
+    w = intx * inty
+    w = w - intz
+    return w
+
 #メイン関数
 if __name__ == "__main__":
     text = input("Enter a string: ")
@@ -20,3 +30,11 @@ if __name__ == "__main__":
     
     print("hoge")
     print("Ooi")
+    
+    #高梨美雪(好きな数字(整数)を3つ入力し、結果をprintする)
+    a = input("put number1: ")
+    b = input("put number2: ")
+    c = input("put number3: ")
+    k = shiki(a,b,c)
+    print("結果：", k)
+    
