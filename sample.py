1ebbed6807a0847e193272a5c900912508468345
LAPTOP-KP2HKT1L\marumoto keita <keita.marumoto@mix-lab.net>
2023-12-28 18:25:48 +0900

    2023/12/28 丸本啓太　ヌメロンの追加

--- sample.py
+++ sample.py
@@ -1,4 +1,11 @@
 import os
+import random
+
+#ヌメロン用
+num=[0,1,2,3,4,5,6,7,8,9]
+ans=random.sample(num,3)
+answer=[]
+count=0
 
 #文字列を反転する
 def reverse_string(input_str):
@@ -37,4 +44,48 @@ if __name__ == "__main__":
     c = input("put number3: ")
     k = shiki(a,b,c)
     print("結果：", k)
+
+    #丸本啓太ヌメロン
+    print("3桁の数字を当てるヌメロン\n")
+    print("値と桁があってるとEAT 値だけ合っているとBITE\n")
+    print("チャンスは3回")
+    while True:
+        while True:
+            print("ヌメロン"+str(count)+"回目の挑戦")
+            a=input("3桁の異なる数字を入力>>\n")
+
+            for i in range(3):
+                answer.append(int(a[i]))
+            
+            if a[0]==a[1]==a[2]:
+                print("異なる数字を入力してね\n")
+                answer.clear()
+            elif a[0]==a[1] or a[0]==a[2] or a[1]==a[2]:
+                print("異なる数字を入力してね\n")
+                answer.clear()
+            else:
+                break
+
+        EAT=0
+        BITE=0
+
+        for i in range(3):
+            if answer[i]==ans[i]:
+                EAT+=1
+            elif answer[i] in ans:
+                BITE+=1
+            else:
+                pass
+        print(str(answer)+" 判定:"+str(EAT)+"EAT,"+str(BITE)+"BITE\n")
+        answer.clear()
+        
+        if EAT==3:
+            print("正解！")
+            break
+        elif count==3:
+            print("失敗")
+            break
+        else:
+            count+=1
+    
     
